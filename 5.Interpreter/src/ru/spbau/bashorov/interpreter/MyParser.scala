5586355b1e75b694074aeede84c497aec6a87cde
bashor <bashor@ya.ru>
2012-11-18 08:48:52 +0400

    small refactoring

--- 5.Interpreter/src/ru/spbau/bashorov/interpreter/MyParser.scala
+++ 5.Interpreter/src/ru/spbau/bashorov/interpreter/MyParser.scala
@@ -4,17 +4,18 @@ import org.parboiled.scala._
 import org.parboiled.errors.{ErrorUtils, ParsingException}
 
 class MyParser extends Parser {
+  def WhiteSpaces: Rule0 = rule { zeroOrMore(anyOf(" \n\r\t\f")) }
   def Digit = rule { "0" - "9" }
   def Digits = rule { oneOrMore(Digit) }
   def DoubleStr = rule { Digits ~ "." ~ Digits }
 
-  def IntNum = rule { WhiteSpace ~ (optional("-") ~ Digits) ~> (s => new AstInt(
+  def IntNum = rule { WhiteSpaces ~ (optional("-") ~ Digits) ~> (s => new AstInt(
       try {
         s.toInt
       } catch {
         case e: NumberFormatException => throw new ParsingException(s"Wrong number format: $s")
-      })) ~ WhiteSpace }
-  def DoubleNum = rule { WhiteSpace ~ (optional("-") ~ DoubleStr) ~> ((s: String) => new AstDouble(s.toDouble)) ~ WhiteSpace }
+      })) ~ WhiteSpaces }
+  def DoubleNum = rule { WhiteSpaces ~ (optional("-") ~ DoubleStr) ~> ((s: String) => new AstDouble(s.toDouble)) ~ WhiteSpaces }
 
   def Number = DoubleNum | IntNum
 
@@ -30,47 +31,43 @@ class MyParser extends Parser {
     | "/" ~ Factor ~~> ((x: AstNode, y) => AstBinOp(x, "/", y).asInstanceOf[AstNode])
     )
   }
-
   def UnOp = rule {
     "-" ~ Expression ~~> ((expr: AstNode) => AstUnOp("-", expr).asInstanceOf[AstNode]) |
     "+" ~ Expression ~~> ((expr: AstNode) => AstUnOp("+", expr).asInstanceOf[AstNode])
   }
-
-  def Factor: Rule1[AstNode] = rule { WhiteSpace ~ (FunCall | Identifier | Number | UnOp | Parens) ~ WhiteSpace}
+  def Factor: Rule1[AstNode] = rule { WhiteSpaces ~ (FunCall | Identifier | Number | UnOp | Parens) ~ WhiteSpaces}
   def Parens = rule { "(" ~ Expression ~ ")" }
 
 
   def IdentifierStart = rule { "a"-"z" | "A" - "Z" | "_" }
-  def Identifier = rule { WhiteSpace ~ (IdentifierStart ~ zeroOrMore(IdentifierStart | Digits)) ~> AstIdentifier ~ WhiteSpace}
+  def Identifier = rule { WhiteSpaces ~ (IdentifierStart ~ zeroOrMore(IdentifierStart | Digits)) ~> AstIdentifier ~ WhiteSpaces}
 
   def Assignment = rule { Identifier ~ "=" ~ Expression ~~> ((id: AstIdentifier, value: AstNode) => AstAssignment(id, value)) }
+
   def FunCall = rule { Identifier ~ "(" ~ zeroOrMore((Assignment | Expression), separator = ",") ~ ")" ~~>
     ((funName: AstIdentifier, params: List[AstNode]) => AstCall(funName, params)) }
 
   def Value = rule { "val" ~ Identifier ~ "=" ~ Expression ~~> ((name: AstIdentifier, expr: AstNode) => AstValue(name, expr))}
+
   def Variable = rule { "var" ~ Identifier ~ "=" ~ Expression ~~> ((name: AstIdentifier, expr: AstNode) => AstVariable(name, expr)) }
 
-  class SignatureHolder(val name: AstIdentifier, val params: List[AstNode], val lastIsRepeated: Boolean)
-
   def FunParam = rule { Assignment | Identifier }
-  def FunSignature = rule { Identifier ~ "(" ~ zeroOrMore(FunParam, separator = ",") ~ optional("*") ~> ((s: String) => !s.isEmpty) ~ ")" ~~>
-    ((name: AstIdentifier, params: List[AstNode], lastIsRepeated: Boolean) => new SignatureHolder(name, params, lastIsRepeated)) ~ WhiteSpace }
-  def Function = rule { "def" ~ FunSignature ~ "=" ~ CommaSeparated ~~>
-    ((fun: SignatureHolder, expr: AstNode) => AstFunction(fun.name, fun.params, fun.lastIsRepeated, expr)) }
+  def FunSignature = rule { Identifier ~ "(" ~ zeroOrMore(FunParam, separator = ",") ~ optional("*") ~> ((s: String) => !s.isEmpty) ~ ")" ~ WhiteSpaces }
+  def Function = rule { "def" ~ FunSignature ~ "=" ~ Operations ~~>
+    ((name: AstIdentifier, params: List[AstNode], lastIsRepeated: Boolean, expr: AstNode) => AstFunction(name, params, lastIsRepeated, expr)) }
 
-  def WhiteSpace: Rule0 = rule { zeroOrMore(anyOf(" \n\r\t\f")) }
-
-  def For = rule { "for" ~ WhiteSpace ~ "(" ~ Identifier ~ ":" ~ Identifier ~")" ~ (Assignment | Expression) ~~>
+  def For = rule { "for" ~ WhiteSpaces ~ "(" ~ Identifier ~ ":" ~ Identifier ~")" ~ (Assignment | Expression) ~~>
     ((it: AstIdentifier, cont: AstIdentifier, body: AstNode) => AstFor(it, cont, body)) }
 
-  def Operation = WhiteSpace ~ (For | Value | Variable | Function | Assignment | Expression) ~ WhiteSpace
-  def CommaSeparated: Rule1[AstNode] = rule {oneOrMore(Operation, separator = ",") ~~>
+  def Operation = WhiteSpaces ~ (For | Value | Variable | Function | Assignment | Expression) ~ WhiteSpaces
+
+  def Operations: Rule1[AstNode] = rule {oneOrMore(Operation, separator = ",") ~~>
     ((operations: List[AstNode]) => if (operations.size == 1) operations(0) else AstComma(operations)) }
-  def Grammar = CommaSeparated ~ EOI
+
+  def Grammar = Operations ~ EOI
 
   def parse(expression: String) : AstNode = {
     val parsingResult = ReportingParseRunner(Grammar).run(expression)
-//    val parsingResult = TracingParseRunner(Grammar).run(expression)
 
     parsingResult.result match {
       case Some(i) => i
